0b218bbce9427512382fb8ca61e58f94b3b8da74
Vijay Daita <vdaita@gmail.com>
2024-06-13 20:45:32 -0500

    changed data preprocessing

--- train.py
+++ train.py
@@ -36,31 +36,8 @@ def generate_text(row):
     return row
 
 ds = ds.map(generate_text, num_proc=20)
+ds = ds.map(lambda samples: tokenizer(samples['text']), batched=True)
 
-def preprocess_function(examples):
-    return tokenizer([" ".join(x) for x in examples["text"]])
-
-tokenized_ds = ds.map(preprocess_function, batched=True, num_proc=4, remove_columns=ds["train"].column_names)
-
-block_size = 1024
-
-def group_texts(examples):
-    # Concatenate all texts.
-    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
-    total_length = len(concatenated_examples[list(examples.keys())[0]])
-    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-    # customize this part to your needs.
-    if total_length >= block_size:
-        total_length = (total_length // block_size) * block_size
-    # Split by chunks of block_size.
-    result = {
-        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-        for k, t in concatenated_examples.items()
-    }
-    result["labels"] = result["input_ids"].copy()
-    return result
-
-lm_dataset = tokenized_ds.map(group_texts, batched=True, num_proc=4)
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
 training_args = TrainingArguments(
@@ -81,8 +58,8 @@ training_args = TrainingArguments(
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=lm_dataset["train"],
-    eval_dataset=lm_dataset["test"],
+    train_dataset=ds["train"],
+    eval_dataset=ds["test"],
     tokenizer=tokenizer,
     data_collator=data_collator,
 )
