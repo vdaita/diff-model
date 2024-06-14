784371867b9c6b12c92300918589bf7c561acacb
Vijay Daita <vdaita@gmail.com>
2024-06-14 17:48:34 -0500

    Based off OPT example

--- train.py
+++ train.py
@@ -1,31 +1,13 @@
-import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, TrainingArguments, Trainer, DataCollatorForLanguageModeling
-from peft import LoraConfig, TaskType
-from peft import get_peft_model
 from datasets import load_dataset
-from accelerate import Accelerator
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2"
+import torch
+import torch.nn as nn
+import bitsandbytes as bnb
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
 
-peft_config = LoraConfig(
-    r=4, 
-    lora_alpha=32, 
-    lora_dropout=0.1, 
-    task_type="CAUSAL_LM", 
-    inference_mode=False, 
-    # target_modules=["q_proj", "v_proj"],
-    bias="none",
-    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "c_fc", "c_proj"]    
-)
-
-model_id = "bigcode/starcoder2-7b"
-
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-tokenizer.pad_token = tokenizer.eos_token
-model = AutoModelForCausalLM.from_pretrained(model_id, load_in_8bit=True, device_map={"": Accelerator().process_index}, torch_dtype=torch.bfloat16)
-
+model_name = "bigcode/starcoder2-15b"
 ds = load_dataset("vdaita/editpackftmulti_inst")
-
-model = get_peft_model(model, peft_config)
-model.print_trainable_parameters()
 
 def generate_text(row):
     patch = row["patch"]
@@ -35,35 +17,89 @@ def generate_text(row):
     row["text"] = f"""# Filename: {filename}\n# File:\n{file}\n# Instructions:\n{inst}\n# Patch:\n```diff\n{patch}\n```"""
     return row
 
-ds = ds.map(generate_text, num_proc=20)
+ds = ds.map(generate_text, num_proc=10)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    load_in_8bit=True,
+    device_map='auto',
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+"""### Post-processing on the model
+
+Finally, we need to apply some post-processing on the 8-bit model to enable training, let's freeze all our layers, and cast the layer-norm in `float32` for stability. We also cast the output of the last layer in `float32` for the same reasons.
+"""
+
+for param in model.parameters():
+  param.requires_grad = False  # freeze the model - train adapters later
+  if param.ndim == 1:
+    # cast the small parameters (e.g. layernorm) to fp32 for stability
+    param.data = param.data.to(torch.float32)
+
+model.gradient_checkpointing_enable()  # reduce number of stored activations
+model.enable_input_require_grads()
+
+class CastOutputToFloat(nn.Sequential):
+  def forward(self, x): return super().forward(x).to(torch.float32)
+model.lm_head = CastOutputToFloat(model.lm_head)
+
+"""### Apply LoRA
+
+Here comes the magic with `peft`! Let's load a `PeftModel` and specify that we are going to use low-rank adapters (LoRA) using `get_peft_model` utility function from `peft`.
+"""
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+from peft import LoraConfig, get_peft_model
+
+config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "c_fc", "c_proj"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
+
+model = get_peft_model(model, config)
+print_trainable_parameters(model)
+
+"""### Training"""
+
+import transformers
+from datasets import load_dataset
+
 ds = ds.map(lambda samples: tokenizer(samples['text']), batched=True)
 
-data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
-
-training_args = TrainingArguments(
-    output_dir="finetuned_starcoder2",
-    warmup_steps=100,
-    per_device_train_batch_size=4,
-    gradient_accumulation_steps=4,
-    learning_rate=2e-4,
-    per_device_eval_batch_size=4,
-    num_train_epochs=2,
-    weight_decay=0.01,
-    evaluation_strategy="epoch",
-    save_strategy="epoch",
-    load_best_model_at_end=True,
-    optim="adafactor",
-    torch_compile=True
-)
-
-trainer = Trainer(
+trainer = transformers.Trainer(
     model=model,
-    args=training_args,
-    train_dataset=ds["train"],
-    eval_dataset=ds["test"],
-    tokenizer=tokenizer,
-    data_collator=data_collator,
+    train_dataset=ds['train'],
+    eval_dataset=ds['test'],
+    args=transformers.TrainingArguments(
+        per_device_train_batch_size=4,
+        gradient_accumulation_steps=4,
+        warmup_steps=100,
+        max_steps=200,
+        learning_rate=2e-4,
+        fp16=True,
+        logging_steps=1,
+        output_dir='outputs'
+    ),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False)
 )
-
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
 trainer.train()
-model.save_pretrained("finetuned_starcoder2")
