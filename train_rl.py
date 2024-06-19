4df6abb393b26a19818ad81516d59d054ec95598
Vijay Daita <vdaita@gmail.com>
2024-06-19 09:30:36 -0500

    rewritten sft and rl train files

--- train_rl.py
+++ train_rl.py
@@ -1,12 +1,37 @@
-from trl import PPOConfig, PPOTrainer, AutoModelForCausalLMWithValueHead
-import ast
-from datasets import load_dataset
-from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
+# Copyright 2022 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from dataclasses import dataclass, field
+from typing import Optional
+
 import torch
-from torch import nn
+from accelerate import Accelerator
+from datasets import load_dataset
+from peft import LoraConfig
 from tqdm import tqdm
-from diff_utils import *
-from peft import LoraConfig, get_peft_model
+from transformers import Adafactor, AutoTokenizer, HfArgumentParser, pipeline
+
+from trl import AutoModelForCausalLMWithValueHead, PPOConfig, PPOTrainer, set_seed
+from trl.core import LengthSampler
+import re
+import ast
+from diff_utils import parse_diff, find_best_match
+
+def extract_code_block_data(md_text, language):
+   # Regular expression pattern for matching diff code blocks
+   pattern = rf'```{language}([\s\S]*?)```'
+   code_blocks = re.findall(pattern, md_text, re.MULTILINE)
+   return code_blocks
 
 def check_python(code): # https://stackoverflow.com/questions/4284313/python-how-to-check-syntax-of-python-file-script-without-executing-it
     try:
@@ -15,96 +40,241 @@ def check_python(code): # https://stackoverflow.com/questions/4284313/python-how
     except SyntaxError:
         return -1
 
-model_name = "./merged_model"
+def evaluate_batch(batch):
+    patch_md = batch["response"]
+    contents = batch["old_contents"]
 
-peft_config = LoraConfig(
+    diff_block = extract_code_block_data(patch_md)
+    sr_blocks = parse_diff(diff_block)
+    sr_blocks = [find_best_match(block.search_block, contents).block for block in sr_blocks]
+    model_line_count = 0
+    
+    for block in sr_blocks:
+        model_line_count += len(block.search_block.splitlines())
+        model_line_count += len(block.replace_block.splitlines())
+        contents = contents.replace(block.search_block, block.replace_block)
+    
+    truth_sr_blocks = parse_diff(batch["patch"])
+    truth_line_count = 0
+    for block in truth_sr_blocks:
+        truth_line_count += len(block.search_block.splitlines())
+        truth_line_count += len(block.replace_block.splitlines())
+
+    rewards.append(torch.tensor(check_python(contents) * min(1, model_line_count/truth_line_count))) # This tuning tries to get the model to generate syntactically valid diffs. 
+    return rewards
+
+tqdm.pandas()
+@dataclass
+class ScriptArguments:
+    """
+    The name of the Casual LM model we wish to fine-tune with PPO
+    """
+
+    # NOTE: gpt2 models use Conv1D instead of Linear layers which are not yet supported in 8 bit mode
+    # models like gpt-neo* models are more suitable.
+    model_name: Optional[str] = field(default="./final_checkpoint/", metadata={"help": "the model name"})
+    tokenizer_name: Optional[str] = field(default="bigcode/starcoder2-15b", metadata={"help": "the tokenizer name"})
+    log_with: Optional[str] = field(default=None, metadata={"help": "use 'wandb' to log with wandb"})
+    learning_rate: Optional[float] = field(default=1.41e-5, metadata={"help": "the learning rate"})
+    output_max_length: Optional[int] = field(default=128, metadata={"help": "maximum length for generation"})
+    mini_batch_size: Optional[int] = field(default=1, metadata={"help": "the PPO minibatch size"})
+    batch_size: Optional[int] = field(default=32, metadata={"help": "the batch size"})
+    ppo_epochs: Optional[int] = field(default=4, metadata={"help": "the number of ppo epochs"})
+    gradient_accumulation_steps: Optional[int] = field(
+        default=4, metadata={"help": "the number of gradient accumulation steps"}
+    )
+    adafactor: Optional[bool] = field(default=False, metadata={"help": "whether to use the adafactor optimizer"})
+    early_stopping: Optional[bool] = field(default=False, metadata={"help": "whether to early stop"})
+    target_kl: Optional[float] = field(default=0.1, metadata={"help": "kl target for early stopping"})
+    reward_baseline: Optional[float] = field(
+        default=0.0,
+        metadata={"help": "a baseline value that is subtracted from the reward"},
+    )
+    batched_gen: Optional[bool] = field(default=False, metadata={"help": "whether to use the batched text gen"})
+    save_freq: Optional[int] = field(default=None, metadata={"help": "n steps to save the model"})
+    output_dir: Optional[str] = field(default="rl-runs/", metadata={"help": "n steps to save the model"})
+    seed: Optional[int] = field(default=0, metadata={"help": "the seed"})
+    steps: Optional[int] = field(default=20000, metadata={"help": "number of epochs"})
+    init_kl_coef: Optional[float] = field(
+        default=0.2,
+        metadata={"help": "Initial KL penalty coefficient (used for adaptive and linear control)"},
+    )
+
+    adap_kl_ctrl: Optional[bool] = field(default=True, metadata={"help": "Use adaptive KL control, otherwise linear"})
+    load_in_8bit: Optional[bool] = field(default=True, metadata={"help": "whether to load the model in 8bit"})
+
+
+parser = HfArgumentParser(ScriptArguments)
+script_args: ScriptArguments = parser.parse_args_into_dataclasses()[0]
+reward_model_name = script_args.reward_model_name
+dataset_name = "vdaita/editpackft_inst"
+config = PPOConfig(
+    steps=script_args.steps,
+    model_name=script_args.model_name,
+    learning_rate=script_args.learning_rate,
+    log_with=script_args.log_with,
+    batch_size=script_args.batch_size,
+    mini_batch_size=script_args.mini_batch_size,
+    gradient_accumulation_steps=script_args.gradient_accumulation_steps,
+    optimize_cuda_cache=True,
+    early_stopping=script_args.early_stopping,
+    target_kl=script_args.target_kl,
+    ppo_epochs=script_args.ppo_epochs,
+    seed=script_args.seed,
+    init_kl_coef=script_args.init_kl_coef,
+    adap_kl_ctrl=script_args.adap_kl_ctrl,
+)
+
+train_dataset = load_dataset(
+   dataset_name, split="train"
+)
+original_columns = train_dataset.column_names
+
+
+tokenizer = AutoTokenizer.from_pretrained(script_args.tokenizer_name, output_max_length=script_args.output_max_length)
+# GPT-2 tokenizer has a pad token, but it is not eos_token by default. We need to set it to eos_token.
+# only for this model.
+
+if getattr(tokenizer, "pad_token", None) is None:
+    tokenizer.pad_token = tokenizer.eos_token
+
+
+# Below is an example function to build the dataset. In our case, we use the IMDB dataset
+# from the `datasets` library. One should customize this function to train the model on
+# its own dataset.
+def build_dataset(
+    tokenizer,
+):
+    """
+    Build dataset for training. This builds the dataset from `load_dataset`, one should
+    customize this function to train the model on its own dataset.
+
+    Args:
+        dataset_name (`str`):
+            The name of the dataset to be loaded.
+
+    Returns:
+        dataloader (`torch.utils.data.DataLoader`):
+            The dataloader for the dataset.
+    """
+
+    num_proc = 24
+
+    def preprocess_function(examples):
+        new_examples = {
+            "query": [],
+            "input_ids": [],
+        }
+
+        for (file, contents, inst) in zip(examples["old_file"], examples["old_contents"], examples["inst"]):
+            query = f"""# Filename: {file}\n# File:\n{contents}\n#Instructions:\n{inst}\n# Patch:\n"""
+            tokenized_question = tokenizer(query, truncation=True)
+            new_examples["query"].append(query)
+            new_examples["input_ids"].append(tokenized_question["input_ids"])
+
+        return new_examples
+
+    ds = train_dataset.map(
+        preprocess_function,
+        batched=True,
+        num_proc=num_proc,
+        # remove_columns=original_columns,
+    )
+    ds = ds.filter(lambda x: len(x["input_ids"]) < script_args.output_max_length, batched=False)
+
+    ds.set_format(type="torch")
+    return ds
+
+
+# We retrieve the dataloader by calling the `build_dataset` function.
+dataset = build_dataset(tokenizer)
+
+
+def collator(data):
+    return {key: [d[key] for d in data] for key in data[0]}
+
+
+# set seed before initializing value head for deterministic eval
+set_seed(config.seed)
+
+# Now let's build the model, the reference model, and the tokenizer.
+current_device = Accelerator().local_process_index
+
+lora_config = LoraConfig(
     r=16,
     lora_alpha=32,
-    target_modules=["q_proj", "v_proj"],
     lora_dropout=0.05,
     bias="none",
-    task_type="CAUSAL_LM"
+    task_type="CAUSAL_LM",
 )
-
 model = AutoModelForCausalLMWithValueHead.from_pretrained(
-    model_name,
-    load_in_8bit=True,
-    device_map='auto',
-    peft_config=peft_config
+    config.model_name,
+    load_in_8bit=script_args.load_in_8bit,
+    device_map={"": current_device},
+    peft_config=lora_config,
 )
 
-main_model_name = "bigcode/starcoder2-7b"
-
-tokenizer = AutoTokenizer.from_pretrained(main_model_name)
-tokenizer.pad_token = tokenizer.eos_token
-
-# Only finetune over python or javascript
-ds = load_dataset("vdaita/editpackftmulti_inst")
-ds = ds["train"]
-
-def add_prompt_and_tokenize(row):
-    row["input_text"] = f"""# Filename: {row['old_file']}\n# File:\n{row['old_contents']}\n# Instructions:\n{row['inst']}\n# Patch:\n```diff"""
-    row["input_ids"] = tokenizer.encode(row["input_text"])
-    return row
-
-ds = ds.map(add_prompt_and_tokenize, num_proc=10)
-
-trainer = PPOTrainer(
-    model=model,
-    config=PPOConfig(
-        model_name="diff-starcoder",
-        learning_rate=1.41e-5
-    ),
-    dataset=ds,
-    tokenizer=tokenizer
+optimizer = None
+if script_args.adafactor:
+    optimizer = Adafactor(
+        filter(lambda p: p.requires_grad, model.parameters()),
+        scale_parameter=False,
+        relative_step=False,
+        warmup_init=False,
+        lr=config.learning_rate,
+    )
+# We then build the PPOTrainer, passing the model, the reference model, the tokenizer
+ppo_trainer = PPOTrainer(
+    config,
+    model,
+    ref_model=None,
+    tokenizer=tokenizer,
+    dataset=dataset,
+    data_collator=collator,
+    optimizer=optimizer,
 )
 
-print("Dataset: ", ds)
+# We then build the sentiment analysis pipeline using our reward model, passing the
+# model name and the sentiment analysis pipeline arguments. Let's also make sure to
+# set the device to the same device as the PPOTrainer.
+device = ppo_trainer.accelerator.device
+if ppo_trainer.accelerator.num_processes == 1:
+    device = 0 if torch.cuda.is_available() else "cpu"  # to avoid a ` pipeline` bug
 
+# We then define the arguments to pass to the `generate` function. These arguments
+# are passed to the `generate` function of the PPOTrainer, which is a wrapper around
+# the `generate` function of the trained model.
 generation_kwargs = {
-    "min_length": -1,
+    # "min_length": -1,
     "top_k": 0.0,
     "top_p": 1.0,
     "do_sample": True,
-    "pad_token_id": tokenizer.eos_token_id,
+    "pad_token_id": tokenizer.pad_token_id,
+    "eos_token_id": 100_000,
 }
+output_min_length = 32
+output_max_length = script_args.output_max_length
+output_length_sampler = LengthSampler(output_min_length, output_max_length)
 
-epochs = 2
+for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
+    if epoch >= config.total_ppo_epochs:
+        break
 
-import re
-def extract_code_block_data(md_text, language):
-   # Regular expression pattern for matching diff code blocks
-   pattern = rf'```{language}([\s\S]*?)```'
-   code_blocks = re.findall(pattern, md_text, re.MULTILINE)
-   return code_blocks
+    question_tensors = batch["input_ids"]
 
-for epoch in tqdm(range(epochs), "epoch: "):
-   for batch in tqdm(trainer.dataloader):
-        query_tensors = batch["input_ids"]
-        response_tensors = trainer.generate(query_tensors, **generation_kwargs)
-        batch["response"] = [tokenizer.decode(r.squeeze()) for r in response_tensors]
-        pairs = list(zip(batch["old_contents"], batch["response"]))
-        rewards = []
-        for (contents, patch_md) in pairs:
-            diff_block = extract_code_block_data(patch_md)
-            sr_blocks = parse_diff(diff_block)
-            sr_blocks = [find_best_match(block.search_block, contents).block for block in sr_blocks]
-            model_line_count = 0
-            
-            for block in sr_blocks:
-                model_line_count += len(block.search_block.splitlines())
-                model_line_count += len(block.replace_block.splitlines())
-                contents = contents.replace(block.search_block, block.replace_block)
-            
-            truth_sr_blocks = parse_diff(batch["patch"])
-            truth_line_count = 0
-            for block in truth_sr_blocks:
-                truth_line_count += len(block.search_block.splitlines())
-                truth_line_count += len(block.replace_block.splitlines())
+    response_tensors = ppo_trainer.generate(
+        question_tensors,
+        return_prompt=False,
+        length_sampler=output_length_sampler,
+        **generation_kwargs,
+    )
+    batch["response"] = tokenizer.batch_decode(response_tensors, skip_special_tokens=True)
 
-            rewards.append(torch.tensor(check_python(contents) * min(1, model_line_count/truth_line_count))) # This tuning tries to get the model to generate syntactically valid diffs. 
+    rewards = evaluate_batch(batch)
 
-        stats = trainer.step(query_tensors, response_tensors, rewards)
-        trainer.log_stats(stats, batch, rewards)
+    # Run PPO step
+    stats = ppo_trainer.step(question_tensors, response_tensors, rewards)
+    ppo_trainer.log_stats(stats, batch, rewards)
 
-trainer.save_pretrained("finetuned_starcoder2_rl")
+    if script_args.save_freq and epoch and epoch % script_args.save_freq == 0:
+        ppo_trainer.save_pretrained(script_args.output_dir + f"step_{epoch}")
--- train_sft.py
+++ train_sft.py
@@ -1,31 +1,203 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+# https://github.com/huggingface/trl/blob/main/examples/research_projects/stack_llama/scripts/supervised_finetuning.py
+
+import argparse
+import os
+
+from accelerate import Accelerator
 from datasets import load_dataset
-from trl import SFTConfig, SFTTrainer, DataCollatorForCompletionOnlyLM
+from peft import LoraConfig
+from tqdm import tqdm
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, logging, set_seed
 
-dataset = load_dataset("vdaita/editpackft_inst", split="train")
+from trl import SFTTrainer
+from trl.trainer import ConstantLengthDataset
 
-model = AutoModelForCausalLM.from_pretrained("bigcode/starcoder2-7b")
-tokenizer = AutoTokenizer.from_pretrained("bigcode/starcoder2-7b")
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, default="")
+    parser.add_argument("--dataset_name", type=str, default="vdaita/editpackft_inst")
+    parser.add_argument("--split", type=str, default="train")
+    parser.add_argument("--size_valid_set", type=int, default=4000)
+    parser.add_argument("--streaming", action="store_true")
+    parser.add_argument("--shuffle_buffer", type=int, default=5000)
 
-def formatting_prompts_func(row):
-    patches = row["patch"]
-    insts = row["inst"]
-    files = row["old_contents"]
-    filenames = row["old_file"]
-    output_texts = []
-    for i in range(len(patches)):
-        output_texts.append(f"""# Filename: {filenames[i]}\n# File:\n{files[i]}\n# Instructions:\n{insts[i]}\n# Patch:\n```diff\n{patches[i]}\n```""")
-    return output_texts
+    parser.add_argument("--seq_length", type=int, default=2048)
+    parser.add_argument("--max_steps", type=int, default=10000)
+    parser.add_argument("--batch_size", type=int, default=4)
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=1)
+    parser.add_argument("--eos_token_id", type=int, default=49152)
 
-response_template = "# Patch:"
-collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
+    parser.add_argument("--learning_rate", type=float, default=1e-4)
+    parser.add_argument("--lr_scheduler_type", type=str, default="cosine")
+    parser.add_argument("--num_warmup_steps", type=int, default=100)
+    parser.add_argument("--weight_decay", type=float, default=0.05)
 
-trainer = SFTTrainer(
-    model,
-    train_dataset=dataset,
-    args=SFTConfig(output_dir="finetuned_starcoder2"),
-    formatting_func=formatting_prompts_func,
-    data_collator=collator,
-)
+    parser.add_argument("--local_rank", type=int, default=0)
+    parser.add_argument("--fp16", action="store_true", default=False)
+    parser.add_argument("--bf16", action="store_true", default=False)
+    parser.add_argument("--gradient_checkpointing", action="store_true", default=False)
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--num_workers", type=int, default=None)
+    parser.add_argument("--output_dir", type=str, default="./checkpoints")
+    parser.add_argument("--log_freq", default=1, type=int)
+    parser.add_argument("--eval_freq", default=1000, type=int)
+    parser.add_argument("--save_freq", default=1000, type=int)
 
-trainer.train()
+    return parser.parse_args()
+
+
+def chars_token_ratio(dataset, tokenizer, nb_examples=400):
+    """
+    Estimate the average number of characters per token in the dataset.
+    """
+    total_characters, total_tokens = 0, 0
+    for _, example in tqdm(zip(range(nb_examples), iter(dataset)), total=nb_examples):
+        text = prepare_sample_text(example)
+        total_characters += len(text)
+        if tokenizer.is_fast:
+            total_tokens += len(tokenizer(text).tokens())
+        else:
+            total_tokens += len(tokenizer.tokenize(text))
+
+    return total_characters / total_tokens
+
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+def prepare_sample_text(row):
+    patch = row["patch"]
+    inst = row["inst"]
+    file = row["old_contents"]
+    filename = row["old_file"]
+    output_text = f"""# Filename: {filename}\n# File:\n{file}\n# Instructions:\n{inst}\n# Patch:\n```diff\n{patch}\n```"""
+    return output_text
+
+def create_datasets(tokenizer, args):
+    dataset = load_dataset(
+        args.dataset_name,
+        split=args.split,
+        use_auth_token=True,
+        num_proc=args.num_workers if not args.streaming else None,
+        streaming=args.streaming,
+    )
+    if args.streaming:
+        print("Loading the dataset in streaming mode")
+        valid_data = dataset.take(args.size_valid_set)
+        train_data = dataset.skip(args.size_valid_set)
+        train_data = train_data.shuffle(buffer_size=args.shuffle_buffer, seed=args.seed)
+    else:
+        dataset = dataset.train_test_split(test_size=0.005, seed=args.seed)
+        train_data = dataset["train"]
+        valid_data = dataset["test"]
+        print(f"Size of the train set: {len(train_data)}. Size of the validation set: {len(valid_data)}")
+
+    chars_per_token = chars_token_ratio(train_data, tokenizer)
+    print(f"The character to token ratio of the dataset is: {chars_per_token:.2f}")
+
+    train_dataset = ConstantLengthDataset(
+        tokenizer,
+        train_data,
+        formatting_func=prepare_sample_text,
+        infinite=True,
+        seq_length=args.seq_length,
+        chars_per_token=chars_per_token,
+    )
+    valid_dataset = ConstantLengthDataset(
+        tokenizer,
+        valid_data,
+        formatting_func=prepare_sample_text,
+        infinite=False,
+        seq_length=args.seq_length,
+        chars_per_token=chars_per_token,
+    )
+    return train_dataset, valid_dataset
+
+
+def run_training(args, train_data, val_data):
+    print("Loading the model")
+
+    lora_config = LoraConfig(
+        r=16,
+        lora_alpha=32,
+        lora_dropout=0.05,
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+
+    train_data.start_iteration = 0
+
+    print("Starting main loop")
+
+    training_args = TrainingArguments(
+        output_dir=args.output_dir,
+        dataloader_drop_last=True,
+        eval_strategy="steps",
+        max_steps=args.max_steps,
+        eval_steps=args.eval_freq,
+        save_steps=args.save_freq,
+        logging_steps=args.log_freq,
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
+        learning_rate=args.learning_rate,
+        lr_scheduler_type=args.lr_scheduler_type,
+        warmup_steps=args.num_warmup_steps,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        gradient_checkpointing=args.gradient_checkpointing,
+        fp16=args.fp16,
+        bf16=args.bf16,
+        weight_decay=args.weight_decay,
+        run_name="llama-7b-finetuned",
+        report_to="wandb",
+        ddp_find_unused_parameters=False,
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model_path, load_in_8bit=True, device_map={"": Accelerator().process_index}
+    )
+
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_data,
+        eval_dataset=val_data,
+        peft_config=lora_config,
+        packing=True,
+    )
+
+    print_trainable_parameters(trainer.model)
+
+    print("Training...")
+    trainer.train()
+
+    print("Saving last checkpoint of the model")
+    trainer.model.save_pretrained(os.path.join(args.output_dir, "final_checkpoint/"))
+
+
+def main(args):
+    tokenizer = AutoTokenizer.from_pretrained(args.model_path)
+    train_dataset, eval_dataset = create_datasets(tokenizer, args)
+    run_training(args, train_dataset, eval_dataset)
+
+
+if __name__ == "__main__":
+    args = get_args()
+    assert args.model_path != "", "Please provide the llama model path"
+
+    set_seed(args.seed)
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    logging.set_verbosity_error()
+
+    main(args)
