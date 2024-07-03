590e8a0db75563598897a2e55e4abccad4f5d731
Vijay Daita <vdaita@gmail.com>
2024-07-03 18:50:47 -0500

    generate_chunked_format makes sense

--- generate_chunked_format.py
+++ generate_chunked_format.py
@@ -5,7 +5,19 @@ import json
 
 parser = get_parser("python")
 
-def chunk_text(text: str, min_chunk_lines=7, max_chunk_lines=10): 
+    
+def find_non_whitespace_indices(s):
+    non_whitespace_indices = [i for i, c in enumerate(s) if not c.isspace()]
+    return non_whitespace_indices[0], non_whitespace_indices[-1]
+
+def chunk_text(text: str, min_chunk_lines=3, max_chunk_lines=5): # CREATE NEW
+    unsplit_text = text
+    start_index, end_index = find_non_whitespace_indices(text)
+    start_chunk = text[:start_index]
+    end_chunk = "" if end_index == len(text) - 1 else text[end_index + 1:]
+
+    text = text.strip()
+
     node = parser.parse(bytes(text, "utf8")).root_node # Tree-sitter doesn't seem to care about indentation :)
     lines = text.splitlines()
     if node.end_point[0] - node.start_point[0] + 1 > max_chunk_lines:
@@ -27,27 +39,29 @@ def chunk_text(text: str, min_chunk_lines=7, max_chunk_lines=10):
                 chunks = []
                 for i in range(len(split_lines) - 1):
                     chunks += chunk_text("\n".join(lines[split_lines[i]:split_lines[i + 1]]))
+                chunks[0] = start_chunk + chunks[0]
+                chunks[-1] += end_chunk
                 return chunks
             
         # Here, there are no further splits that can be made through any of the items
         # Identify a new-line and split after that because that indicates the end of a code "thought". How closely to max_chunk_lines can it be split?
         chunks = [[]]
         for line in lines:
-            if len(line.strip()) == 0:
-                if len(chunks[-1]) >= min_chunk_lines:
-                    chunks.append([]) # Whitespace after the max_chunk_lines? Then, trigger a split
-                chunks[-1].append(line)
-            else:
-                chunks[-1].append(line)
+            if len(line.strip()) == 0 and len(chunks[-1]) >= min_chunk_lines:
+                chunks.append([]) # Whitespace after the max_chunk_lines? Then, trigger a split
+            chunks[-1].append(line)
 
         if len(chunks) >= 2:
             if len(chunks[-1]) < min_chunk_lines:
-                chunks[-2] += ["\n"] + chunks[-1]
+                chunks[-2] += chunks[-1]
                 chunks = chunks[:-1]
 
-        return ["\n".join(chunk) for chunk in chunks]
+        joined_chunks = ["\n".join(chunk) for chunk in chunks]
+        joined_chunks[0] = start_chunk + joined_chunks[0]
+        joined_chunks[-1] = joined_chunks[-1] + end_chunk
+        return joined_chunks
 
-    return [text]
+    return [unsplit_text]
 
 def fix_whitespace_on_chunks(chunks):
     fixed_chunks = []
@@ -55,12 +69,15 @@ def fix_whitespace_on_chunks(chunks):
         if len(chunk.strip()) == 0:
             if len(fixed_chunks) == 0:
                 fixed_chunks.append("")
-            fixed_chunks[-1] += f"\n{chunk}"
+            fixed_chunks[-1] += f"{chunk}"
         else:
             fixed_chunks.append(chunk)
     return fixed_chunks
 
 def make_changes(original_code, new_code):
+    original_code = original_code.rstrip()
+    new_code = new_code.rstrip()
+
     original_lines = original_code.split("\n")
     new_lines = new_code.split("\n")
     
@@ -74,30 +91,33 @@ def make_changes(original_code, new_code):
     diff = diff[first_line_index:]
 
     chunked_text = fix_whitespace_on_chunks(chunk_text(original_code))
-    line_number_to_chunk = {}
-    line_number_to_chunk[-1] = -1
-    current_line_index = 0
-    for chunk_idx, chunk in enumerate(chunked_text):
-        chunk_lines = chunk.splitlines()
-        for line in chunk_lines:
-            line_number_to_chunk[current_line_index] = chunk_idx
-            current_line_index += 1
 
+    assert "\n".join(chunked_text) == original_code
+
+    print(len("\n".join(chunked_text)), len(original_lines))
+
+    print("\n".join(diff))
     original_line_index = 0
-    current_chunk = -1
+    current_chunk_index = 0
     edited_chunks = []
-    current_chunk_lines = []
+    edited_chunk_lines = []
+    original_chunk_lines = []
+
     for line in diff:
         if line.startswith("+"):
-            current_chunk_lines.append(line[1:])
+            edited_chunk_lines.append(line[1:])
         else:
-            if current_chunk != line_number_to_chunk[line]:
-                edited_chunks.append("\n".join(current_chunk_lines))
-                current_chunk_lines = []
+            if "\n".join(original_chunk_lines) == chunked_text[current_chunk_index]: # Check that we have seen the current chunk values before moving on 
+                edited_chunks.append("\n".join(edited_chunk_lines))
+                edited_chunk_lines = []
+                original_chunk_lines = []
+                current_chunk_index += 1
             if line.startswith(" "):
-                current_chunk_lines.append(line[1:])
+                edited_chunk_lines.append(line[1:])
+            original_chunk_lines.append(line[1:])
+
             original_line_index += 1
-    edited_chunks.append("\n".join(current_chunk_lines))
+    edited_chunks.append("\n".join(edited_chunk_lines))
 
     for (original_chunk, edited_chunk) in zip(chunked_text, edited_chunks):
         if original_chunk != edited_chunk:
@@ -107,3 +127,4 @@ if __name__ == "__main__":
     orig_code = open("test_original.txt", "r").read()
     new_code = open("test_new.txt", "r").read()
     print("\n-----\n".join(fix_whitespace_on_chunks(chunk_text(orig_code))))
+    make_changes(orig_code, new_code)
