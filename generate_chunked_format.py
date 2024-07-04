ef54f569da98458335876eb6e5173a94f64f0c18
Vijay Daita <vdaita@gmail.com>
2024-07-04 17:45:37 -0500

    Added applier to generate_chunked_format

--- generate_chunked_format.py
+++ generate_chunked_format.py
@@ -1,6 +1,10 @@
 from tree_sitter import Language, Parser # downgrade to version 0.21.3
 from tree_sitter_languages import get_language, get_parser
 from difflib import unified_diff
+from rapidfuzz import distance
+from dataclasses import dataclass, field
+from heapq import heapify, heappush, heappop, heappushpop
+from functools import total_ordering
 import json
 
 parser = get_parser("python")
@@ -9,7 +13,7 @@ def find_non_whitespace_indices(s):
     non_whitespace_indices = [i for i, c in enumerate(s) if not c.isspace()]
     return non_whitespace_indices[0], non_whitespace_indices[-1]
 
-def chunk_text(text: str, min_chunk_lines=3, max_chunk_lines=5): # CREATE NEW
+def chunk_text(text: str, min_chunk_lines=5, max_chunk_lines=7): # CREATE NEW
     unsplit_text = text
     start_index, end_index = find_non_whitespace_indices(text)
     start_chunk = text[:start_index]
@@ -130,13 +134,14 @@ def generate_compressed_output(original_code, new_code):
         if original_chunk != edited_chunk:
             edited_indices.add(chunk_index)
             edited_indices.add(chunk_index - 1)
-            if chunk_index == 0:
-                edited_indices.add(chunk_index + 1)
+            edited_indices.add(chunk_index + 1)
     edited_indices = list(edited_indices)
     previous_index = 0
     
     output = ""
     for edited_index in edited_indices:
+        if edited_index < 0 or edited_index >= len(edited_chunks):
+            continue
         if edited_index != previous_index + 1:
             first_line = edited_chunks[edited_index].splitlines()[0]
             whitespace = first_line[:len(first_line) - len(first_line.lstrip())]
@@ -145,12 +150,71 @@ def generate_compressed_output(original_code, new_code):
         previous_index = edited_index
     return output
 
-def apply_ellipsis_code(original_code, ellipsis_code):
-    ...
+@total_ordering
+@dataclass
+class Match:
+    norm_distance: float = field(compare=True)
+    start_line: int = field(compare=False)
+    end_line: int = field(compare=False)
 
+    def __eq__(self, other):
+        return self.norm_distance == other.norm_distance
+
+    def __lt__(self, other):
+        return self.norm_distance < other.norm_distance
+    
+    def __gt__(self, other):
+        return self.norm_distance > other.norm_distance
+
+def find_best_matches(original_code, query_code, line_count_tolerance: int = 3, count: int = 1):
+    original_lines = original_code.splitlines()
+    query_lines = query_code.splitlines()
+    best_matches = []
+    heapify(best_matches)
+    for start_line_index in range(len(original_lines)):
+        for end_line_index in range(start_line_index, min(start_line_index + len(query_lines) + line_count_tolerance, len(original_lines))):
+            code_chunk = "\n".join(original_lines[start_line_index:end_line_index + 1])
+            sim_ratio = distance.Levenshtein.normalized_similarity(code_chunk, query_code)
+            if len(best_matches) < count: # Just makes it easier 
+                heappush(best_matches, Match(sim_ratio, start_line_index, end_line_index))
+            else:
+                heappushpop(best_matches, Match(sim_ratio, start_line_index, end_line_index))
+    return list(best_matches)       
+
+def apply_ellipsis_code(original_code, ellipsis_code): # Consider adding starting and finished lines to the code. 
+    chunks = [[]]
+    for line in ellipsis_code.splitlines():
+        line_wo_whitespace = ''.join([ch for ch in line if not(ch.isspace())])
+        if line_wo_whitespace == "@@...@@":
+            chunks.append([])
+        else:
+            chunks[-1].append(line)
+    chunks = ["\n".join(lines) for lines in chunks]
+
+    remaining_original_code = original_code # TODO: slowly reduce the space being searched as you work your way down the file
+    changed_code = original_code
+
+    for chunk in chunks:
+        chunk_lines = chunk.splitlines()
+        best_start_chunk = Match(-1, -1, -1)
+        best_end_chunk = Match(-1, -1, -1)
+        for start_chunk_length in range(2, 11):
+            start_chunk = "\n".join(chunk_lines[:max(start_chunk_length, len(chunk_lines))])
+            best_start_chunk = max(best_start_chunk, find_best_matches(original_code, start_chunk)[0])
+        for end_chunk_length in range(2, 11):
+            end_chunk = "\n".join(chunk_lines[max(0, len(chunk_lines) - end_chunk_length):])
+            best_end_chunk = max(best_end_chunk, find_best_matches(original_code, end_chunk)[0])
+        original_chunk = "\n".join(original_code.splitlines()[best_start_chunk.start_line:best_end_chunk.end_line + 1])
+        changed_code = changed_code.replace(original_chunk, chunk)
+    
+    return changed_code
 if __name__ == "__main__":
     orig_code = open("test_original.txt", "r").read()
     new_code = open("test_new.txt", "r").read()
     # print("\n-----\n".join(fix_whitespace_on_chunks(chunk_text(orig_code))))
     # make_changes(orig_code, new_code)
-    print(generate_compressed_output(orig_code, new_code))
+    compressed_output = generate_compressed_output(orig_code, new_code)
+    print(compressed_output)
+    print("-----")
+    applied_code = apply_ellipsis_code(orig_code, compressed_output)
+    print(applied_code)
